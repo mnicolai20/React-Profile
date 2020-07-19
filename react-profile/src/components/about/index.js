43777ca63c905b16a6d02b2859157c040bc56a53
nicolai <mattrnicolai@gmail.com>
2020-07-19 12:49:34 -0400

    navbar, about page and portfolio route functioning. No styling added

---
+++ react-profile/src/components/about/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class AboutDetail extends Component {
+    state = {
+        
+    }
+}
---
+++ react-profile/src/components/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        This is the portfolio page
+       </p>
+    </div>
+  );
+}
+
+export default Portfolio;
